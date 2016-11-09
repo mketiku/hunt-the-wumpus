f0166fa959ce8a2114e61b03b9874305031e6a92
Ketiku <mketiku@butler.edu>
2016-11-09 15:50:07 -0500

    update room to mark visits

--- src/room.py
+++ src/room.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python3
+
+
 class room(object):
     '''A single room in the wumpus cave
     '''
 
-    def __init__(self,name, t1, t2,t3, room):
+    def __init__(self, name, t1, t2, t3, room):
         '''
         Constructor method
         '''
-        self.name= name
-        self.location = room 
+        self.name = name
+        self.location = room
         self.visited = False
         self.contains_gold = False
-        self.adjacent = [t1,t2,t3]
+        self.adjacent = [t1, t2, t3]
 
     def get_adjacent(self):
         return self.adjacent
@@ -23,6 +26,12 @@ class room(object):
 
     def contains_gold(self):
         return self.contains_gold
-    
+
     def remove_gold(self):
-        return self.contains_gold = False
+        return self.contains_gold = False #TODO 
+
+    # def is_visited(self):
+    #     return self.visited
+
+    # def mark_visited(self):
+    #     self.visited = true
